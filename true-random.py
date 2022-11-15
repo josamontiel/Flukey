e097bbfe88198904f2c334fe11214b53564b73a7
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-11-15 11:52:57 +0000

    added some aesthetic changes
    Cleaned up the printed messages with some line breaks for a much more polished look

--- true-random.py
+++ true-random.py
@@ -26,35 +26,35 @@ def progress_bar():
 	    time.sleep(0.01)
 
 # Prompt user to make selection
-choose_option = int(input("What would you like to generate?\n 1. A PIN number.\n 2. A Password.\n 3. A Passphrase. \n"))
+choose_option = int(input("\nWhat would you like to generate?\n\n (1) PIN number\n (2) Password\n (3) Passphrase\n\n"))
     
 # Function for pin number
 def pin_gen():
-       pin_len = int(input("Length of your new PIN: ")) 
+       pin_len = int(input("\nLength of your new PIN: ")) 
        numbers = string.digits
        pin_num = ''.join(secrets.choice(numbers)for i in range(int(pin_len)))
        progress_bar() 
-       print(pin_num)
+       print("\nHere is your PIN: \n\n\n" + pin_num + "\n\n")
 
 # Function for password
 def password_gen():
-        password_len = int(input("Length of password: "))
+        password_len = int(input("\nLength of password: "))
 
         lower_alpha = string.ascii_lowercase + string.digits
         upper_alpha = string.ascii_uppercase + string.digits
     
         password = ''.join(secrets.choice(lower_alpha + upper_alpha) for i in range (password_len))
         progress_bar() 
-        print(password)
+        print("\nHere is your Password: \n\n\n" + password + "\n\n")
 
 # Function for passphrase
 def passphrase_gen():
         with open('/usr/share/dict/words') as f:
-                passphrase_len = int(input("How many words: "))
+                passphrase_len = int(input("\nHow many words: "))
                 words = [word.strip() for word in f]
                 passphrase = ' '.join(secrets.choice(words) for i in range(passphrase_len))
                 progress_bar() 
-                print(passphrase)
+                print("\nHere is your Passphrase: \n\n\n" + passphrase + "\n\n")
 
 
 def gen_choices():
