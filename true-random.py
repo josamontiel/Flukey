6250b07ce293192947153556352b36ea32551f11
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-11-28 14:52:22 +0000

    left some comments for future function
    Left myself some comments for a function I eventually want to implement for creating a text file with the generated output

--- true-random.py
+++ true-random.py
@@ -1,5 +1,6 @@
 ##### TRUE RANDOM #####
 
+#Library for generating random/secure numbers or letters
 import secrets
 import string
 
@@ -13,6 +14,7 @@ def progress_bar():
                    desc="Loading…",
 		           ascii=False, ncols=75):
 	    time.sleep(0.01)
+      
 
 # Prompt user to make selection
 choose_option = int(input("\nWhat would you like to generate?\n\n (1) PIN number\n (2) Password\n (3) Passphrase\n\n"))
@@ -24,6 +26,7 @@ def pin_gen():
     pin_num = ''.join(secrets.choice(numbers)for i in range(int(pin_len)))
     progress_bar() 
     print(f"\nHere is your PIN: \n\n\n{pin_num}\n\n")
+    # create_text_file()
 
 # Function for password
 def password_gen():
@@ -36,6 +39,7 @@ def password_gen():
         password = ''.join(secrets.choice(lower_alpha + upper_alpha + numbers)  for i in range (password_len))
         progress_bar() 
         print(f"\nHere is your Password: \n\n\n{password}\n\n")
+        # create_text_file()
 
 # Function for passphrase
 def passphrase_gen():
@@ -44,19 +48,34 @@ def passphrase_gen():
                 words = [word.strip() for word in f]
                 passphrase = ' '.join(secrets.choice(words) for i in range(passphrase_len))
                 progress_bar() 
-                print(f"\nHere is your Passphrase: \n\n\n{passphrase}\n\n")
+                print(f"\nHere is your Passphrase: \n\n\n{passphrase.title()}\n\n")
+                # create_text_file()
 
 
 def gen_choices():
+    
+    c = choose_option
 
-    if choose_option == int('1'):
+    if c == int('1'):
         return pin_gen()
-    elif choose_option == int('2'):
+    elif c == int('2'):
         return password_gen()
-    elif choose_option == int('3'):
+    elif c == int('3'):
         return passphrase_gen()
     else:
-        return choose_option
+        return c
+    
+# def make_my_file():
+#     make_file = input("Would you like to copy this to a text file (Y/N): ")
+#          # Function to create output file
+#     def create_text_file():
+#         with open("output.txt", "w") as my_file:
+#             my_file.write()
 
+#     if make_file == 'Y' or 'y':
+#         return create_text_file()
+#     else:
+#         return choose_option
+   
 gen_choices()
-
+# make_my_file()
