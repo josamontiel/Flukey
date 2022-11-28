330c86c4464847ba695a8b94e55214c8326cd4f2
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-11-28 13:50:25 +0000

    fstring, removed todo
    used f string instead of concatenating strings w vars and removed TODO

--- true-random.py
+++ true-random.py
@@ -1,26 +1,13 @@
 ##### TRUE RANDOM #####
 
-# Generate truly random PINs, Passwords and Passphrases 
-
-# HOW IT WORKS: 
-# 
-#  You will be prompted to select between a PIN, Password or Passphrase
-#  Upon selection you will be asked for the length of your choice
-#  
-# The CLI will spit out a random PIN/Password or Passphrase
-# 
-# END #
-
-#Library for generating random/secure numbers or letters
 import secrets
 import string
-# Install wordlist to remove dependency on local machine dictionary
-# import wordlist
 
 #libraries for progressbar
 from tqdm import tqdm
 import time
 
+# Progress bar animation
 def progress_bar():
     for i in tqdm (range (101),
                    desc="Loading…",
@@ -32,22 +19,23 @@ choose_option = int(input("\nWhat would you like to generate?\n\n (1) PIN number
     
 # Function for pin number
 def pin_gen():
-       pin_len = int(input("\nLength of your new PIN: ")) 
-       numbers = string.digits
-       pin_num = ''.join(secrets.choice(numbers)for i in range(int(pin_len)))
-       progress_bar() 
-       print("\nHere is your PIN: \n\n\n" + pin_num + "\n\n")
+    pin_len = int(input("\nLength of your new PIN: ")) 
+    numbers = string.digits
+    pin_num = ''.join(secrets.choice(numbers)for i in range(int(pin_len)))
+    progress_bar() 
+    print(f"\nHere is your PIN: \n\n\n{pin_num}\n\n")
 
 # Function for password
 def password_gen():
         password_len = int(input("\nLength of password: "))
 
-        lower_alpha = string.ascii_lowercase + string.digits
-        upper_alpha = string.ascii_uppercase + string.digits
-    
-        password = ''.join(secrets.choice(lower_alpha + upper_alpha) for i in range (password_len))
-        progress_bar()
-        print("\nHere is your Password: \n\n\n" + password + "\n\n")
+        lower_alpha = string.ascii_lowercase 
+        upper_alpha = string.ascii_uppercase
+        numbers = string.digits
+
+        password = ''.join(secrets.choice(lower_alpha + upper_alpha + numbers)  for i in range (password_len))
+        progress_bar() 
+        print(f"\nHere is your Password: \n\n\n{password}\n\n")
 
 # Function for passphrase
 def passphrase_gen():
@@ -56,7 +44,7 @@ def passphrase_gen():
                 words = [word.strip() for word in f]
                 passphrase = ' '.join(secrets.choice(words) for i in range(passphrase_len))
                 progress_bar() 
-                print("\nHere is your Passphrase: \n\n\n" + passphrase + "\n\n")
+                print(f"\nHere is your Passphrase: \n\n\n{passphrase}\n\n")
 
 
 def gen_choices():
@@ -71,3 +59,4 @@ def gen_choices():
         return choose_option
 
 gen_choices()
+
