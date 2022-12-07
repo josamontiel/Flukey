cfe44f9121866c32c67f3a8eb7fb57c3854ac148
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-12-07 17:29:56 +0000

    Add files via upload

---
+++ package/src/exec.sh
@@ -0,0 +1,13 @@
+
+#!/bin/bash
+<<com
+Shell script to run program from Terminal.
+
+Aim is to have this script execute the generator from the terminal, without the need of entering extra commands.
+com
+
+python3 true-random.py
+
+
+
+
