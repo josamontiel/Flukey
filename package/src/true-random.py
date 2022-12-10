1b0238b04372089e1fa2fef408a2344f4ca778f0
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-12-10 20:27:52 +0000

    Added qr code functionality
    Now with any of the types of codes you generate you will be given the option to save it on to a PNG in the form of a qr code.

--- package/src/true-random.py
+++ package/src/true-random.py
@@ -5,6 +5,8 @@
 #Library for generating random/secure numbers or letters
 import secrets
 import string
+import qrcode
+import qrcode.image.svg
 
 #libraries for progressbar
 from tqdm import tqdm
@@ -16,6 +18,8 @@ def progress_bar():
                    desc="Loading…",
 		           ascii=False, ncols=75):
 	    time.sleep(0.01)
+     
+
       
 
 # Prompt user to make selection
@@ -28,6 +32,22 @@ def pin_gen():
     pin_num = ''.join(secrets.choice(numbers)for i in range(int(pin_len)))
     progress_bar() 
     print(f"\nHere is your PIN: \n\n\n{pin_num}\n\n")
+    
+    def qr_gen():
+    
+        save_to_qr = print(input("Would you like to save this PIN on a QR code? (Y/N): "))
+    
+        type(save_to_qr)
+    
+        save_to_qr = qrcode.make(f"{pin_num}")
+    
+        if save_to_qr == "Y" or "y":
+            return save_to_qr.save("rename.png")
+        else:
+            return 
+    qr_gen()
+    
+    
     # create_text_file()
 
 # Function for password
@@ -44,13 +64,28 @@ def password_gen():
         password_with_spec_chars = ''.join(secrets.choice(password_without_spec_chars + special_chars)  for i in range (password_len))
         
         def yes_or_no():
-            if with_or_without_special_characters == "Y":
+            if with_or_without_special_characters == "Y" or with_or_without_special_characters == "y":
                 return password_with_spec_chars
             else:
                 return password_without_spec_chars
             
         progress_bar() 
         print(f"\nHere is your Password: \n\n\n{yes_or_no()}\n\n")
+        
+        def qr_gen():
+    
+            save_to_qr = print(input("Would you like to save this on a QR code? (Y/N): "))
+    
+            type(save_to_qr)
+    
+            save_to_qr = qrcode.make(f"{yes_or_no()}")
+    
+            if save_to_qr == "Y" or "y":
+                return save_to_qr.save("rename.png")
+            else:
+                return 
+        qr_gen()
+       
         # create_text_file()
 
 # Function for passphrase
@@ -58,16 +93,38 @@ def passphrase_gen():
         with open('/usr/share/dict/words') as f:
                 passphrase_len = int(input("\nHow many words: "))
                 words = [word.strip() for word in f]
-                passphrase = ' \n'.join(secrets.choice(words) for i in range(passphrase_len))
+                passphrase = ' \n'.join(secrets.choice(words).title() for i in range(passphrase_len))
                 progress_bar() 
-                print(f"\nHere is your Passphrase: \n\n\n{passphrase.title()}\n\n")
+                print(f"\nHere is your Passphrase: \n\n\n{passphrase}\n\n")
+                
+        def qr_gen():
+    
+            save_to_qr = print(input("Would you like to save this on a QR code? (Y/N): "))
+    
+            type(save_to_qr)
+    
+            save_to_qr = qrcode.make(f"{passphrase}")
+    
+            if save_to_qr == "Y" or "y":
+                return save_to_qr.save("rename.png")
+            else:
+                return 
+        qr_gen()
+                
                 # create_text_file()
 
 
+
+    
+    
+
+    
 def gen_choices():
     
+      
     c = choose_option
-
+    
+    
     if c == int('1'):
         return pin_gen()
     elif c == int('2'):
@@ -77,17 +134,18 @@ def gen_choices():
     else:
         return c
     
-# def make_my_file():
-#     make_file = input("Would you like to copy this to a text file (Y/N): ")
-#          # Function to create output file
-#     def create_text_file():
-#         with open("output.txt", "w") as my_file:
-#             my_file.write()
-
-#     if make_file == 'Y' or 'y':
-#         return create_text_file()
+# def qr_gen():
+    
+#     save_to_qr = print(input("Would you like to save this on a QR code? (Y/N): "))
+    
+#     type(save_to_qr)
+    
+#     save_to_qr = qrcode.make(f"{}")
+    
+#     if save_to_qr == "Y" or "y":
+#         return save_to_qr.save("rename.png")
 #     else:
-#         return choose_option
-   
+#         return 
+
 gen_choices()
-# make_my_file()
+
