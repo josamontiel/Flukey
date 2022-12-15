67b065cfd64a323c1b8d2d92379da5fee59ba02a
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-12-15 17:02:19 +0000

    Update exec.sh
    updated file to prepare for separation of files for a bash file that runs program

--- package/src/exec.sh
+++ package/src/exec.sh
@@ -6,7 +6,26 @@ Shell script to run program from Terminal.
 Aim is to have this script execute the generator from the terminal, without the need of entering extra commands.
 com
 
-python3 true-random.py
+<<instructions
+    Have args for each functions:
+        - password
+        - pin
+        - passphrase
+    Example: run <arg: password> run password.py
+
+    separate true random into 3 files for separation of actions 
+instructions
+
+###################################################################
+
+pin=$(filename that corresponds with file)
+if [[arg==pin]]
+./pin.py
+else if [[arg==password]]
+./password.py
+else if [[arg==passphrase]]
+./passphrase.py
+fi
 
 
 
