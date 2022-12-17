48ec1defa9581e410fb6f81bb5207e8b21fa174a
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-12-17 23:06:35 +0000

    Add files via upload

---
+++ package/src/documentation.py
@@ -0,0 +1,6 @@
+'''
+Help menu that will be built into the main app.
+TODO:
+1. Convert documentation to python to allow for ease of navigation in the terminal
+2. Incorporate help function into main menu of file
+'''
