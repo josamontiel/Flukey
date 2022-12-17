bb4c74e4d8141666825744a280851e3ab43482e4
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-12-17 18:00:51 +0000

    new menu format
    Updated the menu format for a cleaner multi line string

--- package/src/true-random.py
+++ package/src/true-random.py
@@ -1,140 +1,108 @@
 #!/usr/bin/python3
 
-##### TRUE RANDOM #####
 
-#Library for generating random/secure numbers or letters
+# import typing
+# import os
+# import sys
+# import pydoc
+
+# libs for randomization
 import secrets
 import string
+# qr code libraries
 import qrcode
 import qrcode.image.svg
-
-#libraries for progressbar
+# libraries for progressbar
 from tqdm import tqdm
 import time
 
-# Progress bar animation
+
 def progress_bar():
-    for i in tqdm (range (101),
-                   desc="Loading…",
-		           ascii=False, ncols=75):
-	    time.sleep(0.01)
-     
+    for i in tqdm(range(101),
+                  desc="Loading…",
+                  ascii=False, ncols=75):
+        time.sleep(0.01)
 
-class QR_CODE:
-    pass
 
-# Prompt user to make selection
-# choose_option = int(input("\nWhat would you like to generate?\n\n (1) PIN number\n (2) Password\n (3) Passphrase\n\n"))
-    
-# Function for pin number
-def pin_gen():
-    pin_len = int(input("\nLength of your new PIN: ")) 
-    numbers = string.digits
-    pin_num = ''.join(secrets.choice(numbers)for i in range(int(pin_len)))
-    progress_bar() 
-    print(f"\nHere is your PIN: \n\n\n{pin_num}\n\n")
-    
-    def qr_gen():
-    
-        save_to_qr = print(input("Would you like to save this PIN on a QR code? (Y/N): "))
-    
+class qr_Code:
+    def generate(code_type):
+        save_to_qr = input("Would you like to save this on a QR code? (Y/N): ")
         type(save_to_qr)
-    
-        save_to_qr = qrcode.make(f"{pin_num}")
-    
+        save_to_qr = qrcode.make(f"{code_type}")
+
         if save_to_qr == "Y" or "y":
-            return save_to_qr.save("rename.png")
+            return save_to_qr.save("newfile.png")
         else:
-            return 
-    qr_gen()
+            pass
 
-# Function for password
-def password_gen():
-        password_len = int(input("\nLength of password: "))
-        with_or_without_special_characters = input("Would you like to include special characters? (Y/N): ")
 
-        lower_alpha = string.ascii_lowercase 
-        upper_alpha = string.ascii_uppercase
-        numbers = string.digits
-        special_chars = string.punctuation
+def pin_generate():
+    pin_len = int(input("\nLength of your new PIN: "))
+    numbers = string.digits
+    pin_num = ''.join(secrets.choice(numbers) for i in range(int(pin_len)))
+    progress_bar()
+    print(f"\nHere is your PIN: \n\n\n{pin_num}\n\n")
+    qr_Code.generate(pin_num)
 
-        password_without_spec_chars = ''.join(secrets.choice(lower_alpha + upper_alpha + numbers)  for i in range (password_len))
-        password_with_spec_chars = ''.join(secrets.choice(password_without_spec_chars + special_chars)  for i in range (password_len))
-        
-        def yes_or_no():
-            if with_or_without_special_characters == "Y" or with_or_without_special_characters == "y":
-                return password_with_spec_chars
-            else:
-                return password_without_spec_chars
-            
-        progress_bar() 
-        print(f"\nHere is your Password: \n\n\n{yes_or_no()}\n\n")
-        
-        def qr_gen():
-    
-            save_to_qr = print(input("Would you like to save this on a QR code? (Y/N): "))
-    
-            type(save_to_qr)
-    
-            save_to_qr = qrcode.make(f"{yes_or_no()}")
-    
-            if save_to_qr == "Y" or "y":
-                return save_to_qr.save("rename.png")
-            else:
-                return 
-        qr_gen()
 
-# Function for passphrase
-def passphrase_gen():
-        with open('/usr/share/dict/words') as f:
-                passphrase_len = int(input("\nHow many words: "))
-                words = [word.strip() for word in f]
-                passphrase = ' \n'.join(secrets.choice(words).title() for i in range(passphrase_len))
-                progress_bar() 
-                print(f"\nHere is your Passphrase: \n\n\n{passphrase}\n\n")
-                
-        def qr_gen():
-    
-            save_to_qr = print(input("Would you like to save this on a QR code? (Y/N): "))
-    
-            type(save_to_qr)
-    
-            save_to_qr = qrcode.make(f"{passphrase}")
-    
-            if save_to_qr == "Y" or "y":
-                return save_to_qr.save("rename.png")
-            else:
-                return 
-        qr_gen()
-                
-    
-# def gen_choices():
-    
-      
-#     c = choose_option
-    
-    
-#     if c == int('1'):
-#         return pin_gen()
-#     elif c == int('2'):
-#         return password_gen()
-#     elif c == int('3'):
-#         return passphrase_gen()
-#     else:
-#         return c
-# gen_choices()
+def password_generate():
+    password_len = int(input("\nLength of password: "))
+    with_or_without_special_characters = input(
+        "Would you like to include special characters? (Y/N): ")
 
-class EXECUTE():
-    one = int('1')
-    two = int('2')
-    three = int('3')
+    lower_alpha = string.ascii_lowercase
+    upper_alpha = string.ascii_uppercase
+    numbers = string.digits
+    special_chars = string.punctuation
 
-choose_option = int(input("\nWhat would you like to generate?\n\n (1) PIN number\n (2) Password\n (3) Passphrase\n\n"))
+    password_without_spec_chars = ''.join(secrets.choice(
+        lower_alpha + upper_alpha + numbers) for i in range(password_len))
+    password_with_spec_chars = ''.join(secrets.choice(
+        password_without_spec_chars + special_chars) for i in range(password_len))
 
-match choose_option:
-    case EXECUTE.one:
-        print(pin_gen())
-    case EXECUTE.two:
-        print(password_gen())
-    case EXECUTE.three:
-        print(passphrase_gen())    
+    def yes_or_no():
+        if with_or_without_special_characters == "Y" or with_or_without_special_characters == "y":
+            return password_with_spec_chars
+        else:
+            return password_without_spec_chars
+
+    progress_bar()
+    print(f"\nHere is your Password: \n\n\n{yes_or_no()}\n\n")
+    qr_Code.generate(yes_or_no())
+
+
+def passphrase_generate():
+    with open('/usr/share/dict/words') as f:
+        passphrase_len = int(input("\nHow many words: "))
+        words = [word.strip() for word in f]
+        passphrase = ' \n'.join(secrets.choice(words).title()
+                                for i in range(passphrase_len))
+        progress_bar()
+        print(f"\nHere is your Passphrase: \n\n\n{passphrase}\n\n")
+        qr_Code.generate(passphrase)
+
+
+def main_menu():
+    print("""
+        Welcome to True Random!!!
+-->Generate truly random PINs/Passwords/Passphrases<--
+
+Select an option below:
+-- PIN
+
+-- Password
+
+-- Passphrase
+           """)
+    selection = input("> ").lower()
+    if selection == 'pin':
+        return pin_generate()
+    elif selection == 'password':
+        return password_generate()
+    elif selection == 'passphrase':
+        return passphrase_generate()
+    else:
+        print("Please make a valid selection!")
+
+
+main_menu()
