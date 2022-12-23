c2a9c087db24d77a43d55f75042fbb43ea68c366
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-12-23 22:02:31 +0000

    Delete documentation.py

--- package/src/documentation.py
+++
@@ -1,6 +0,0 @@
-'''
-Help menu that will be built into the main app.
-TODO:
-1. Convert documentation to python to allow for ease of navigation in the terminal
-2. Incorporate help function into main menu of file
-'''
