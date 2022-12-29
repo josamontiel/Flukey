17c902dd794e7988f8897ef9f470e3748b16a36b
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2022-12-29 17:58:39 +0000

    added functionality and notes
    Added more notes to describe what is going on with all of the functions and why certain libraries are imported. 
    
    Also added a quantity function to determine how manty iterations of passwords that a user can generate.

--- package/src/true_random.py
+++ package/src/true_random.py
@@ -3,15 +3,15 @@
 # libs for randomization
 import secrets
 import string
-
-
 # libraries for progressbar
 from tqdm import tqdm
 import time
-
+# QR code generation function is left in an external file as I felt it made the code in the main file look cleaner.
 import qr_gen_file
+# Library that allows for shell styling
 import click
 
+# colors for animations of the CLI when running the program
 all_colors = (
     "black",
     "red",
@@ -33,6 +33,10 @@ all_colors = (
 
 
 def progress_bar():
+    """
+    Progress bar animation
+    adds a progressbar which doesn't really serve a function to the program itself, but it does look cool.
+    """
     for i in tqdm(range(101),
                   desc="Loading…",
                   ascii=False, ncols=75):
@@ -49,16 +53,20 @@ def pin():
 
 
 def password():
+    """
+    Password function has a coule of built-in functions:
+    - choosing between special chars in PWs or not
+    - determining the amount of different password iterations you would like
+    """
     password_len = int(input("\nLength of password: "))
     with_or_without_special_characters = input(
         "Would you like to include special characters? (Y/N): ")
-
+    quantity_of_passwords_to_generate = int(input("How many different passwords would you like to generate? "))
     characters = string.ascii_letters
     numbers = string.digits
     special_chars = string.punctuation
 
     def yes_or_no():
-
         password_without_spec_chars = ''.join(secrets.choice(
             characters + numbers) for i in range(password_len))
         password_with_spec_chars = ''.join(secrets.choice(
@@ -68,7 +76,10 @@ def password():
             return password_with_spec_chars
         else:
             return password_without_spec_chars
-
+    '''
+    The below if statement will print off a warning to the user that the password
+    they have selected is very short and increases the likelihood of being cracked
+    '''
     if password_len <= 12:
         click.echo(click.style("""
             Your password is pretty weak, 
@@ -82,13 +93,29 @@ def password():
             yes_or_no()
         else:
             return password()
-        
+
     progress_bar()
-    click.echo(click.style(f"\nHere is your Password:\n\n{yes_or_no()}\n", fg=all_colors[-3]))
+
+    def amount_of_passwords():
+        """
+         Function that instructs the program, iterations of passwords to generate depends on the number selected.
+        """
+        for pwd in range(quantity_of_passwords_to_generate):
+            passwords = ''
+            for c in range(password_len):
+                passwords += yes_or_no()
+            click.echo(yes_or_no())
+
+    click.echo(click.style(f"\nTA-DA:\n\n{amount_of_passwords()}\n", fg=all_colors[-3]))
+
     qr_gen_file.generate()
 
 
 def passphrase():
+    """
+    This function draws words that is stored in a computers internal dictionary.
+    So I imagine that the words may vary depending on the language?
+    """
     with open('/usr/share/dict/words') as f:
         passphrase_len = int(input("\nHow many words: "))
         words = [word.strip() for word in f]
