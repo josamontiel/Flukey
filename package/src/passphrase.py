a9f16cc0e662a0f7a9d5c4cb13ac466d28a28f0b
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2023-01-15 15:44:34 +0000

    Update passphrase.py

--- package/src/passphrase.py
+++ package/src/passphrase.py
@@ -1,5 +1,4 @@
 import secrets
-import string
 from qr_gen_file import *
 from progressbar import *
 from shell_functionality import *
@@ -9,8 +8,14 @@ def passphrase():
     
 
     """
-    This function draws words that is stored in a computers internal dictionary.
+    This function draws words that are stored in a machines internal dictionary.
     So I imagine that the words may vary depending on the language?
+    """
+    """
+    Filepath to the dictionary is opened,
+    the user is then asked how many word they would like their
+    passphrase to be, following that, they are then 
+    presented with the final output of their selection.
     """
     with open('/usr/share/dict/words') as f:
         passphrase_len = int(input("\nHow many words: "))
@@ -19,3 +24,5 @@ def passphrase():
         progress_bar()
         print(f"\nHere is your Passphrase: \n\n\n{passphrase_gen}\n\n")
         qr_code_generate()
+        
+# passphrase()
