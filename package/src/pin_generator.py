f2d9f367f4fcba3e908d9ef0df9d975028674df4
Joseph A. M <91287801+josamontiel@users.noreply.github.com>
2023-01-15 15:55:12 +0000

    Update pin_generator.py

--- package/src/pin_generator.py
+++ package/src/pin_generator.py
@@ -5,6 +5,13 @@ from qr_gen_file import *
 from progressbar import progress_bar
 
 def pin():
+    """
+    The functionality for the pin is pretty straightforward,
+    the pin length is selected and it takes the digits method 
+    from the string library to give us '0-9'.
+    The secrets.choice method allows us to get a random selection
+    based on the length of our pin we selected, giving us the finished product
+    """
     pin_len = int(input("\nLength of your new PIN: "))
     numbers = string.digits
     pin_num = ''.join(secrets.choice(numbers) for i in range(int(pin_len)))
